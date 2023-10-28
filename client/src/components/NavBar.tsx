3aa4c02674c0e0a07ae98b8fe6398ee3011ec0f4
Neeraj Patil <patilneeraj2212@gmail.com>
2023-10-28 13:50:51 +0530

    Added NavBar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { useContext } from "react";
+import { UserContext } from "@src/contexts/UserContext";
+import { useNavigate } from "react-router-dom";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  const { user, logout } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  return (
+    <nav className="flex gap-x-4 bg-secondary justify-between items-center p-5 text-lg">
+      <div className="text-lg">
+        <p>Welcome {user?.username}</p>
+      </div>
+      <div className="flex items-center justify-center">
+        <button
+          className="bg-primary px-4 py-2 rounded-md font-bold"
+          onClick={() => {
+            logout();
+            navigate("/login");
+          }}
+        >
+          Logout
+        </button>
+      </div>
+    </nav>
+  );
 }
